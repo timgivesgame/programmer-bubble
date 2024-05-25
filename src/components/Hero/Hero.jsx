4db2427e86e1551981f740e611d91f10d645a2c2
timgivesgame <142540848+timgivesgame@users.noreply.github.com>
2024-05-25 13:45:03 -0400

    Refactor Hero button to replace Home component with SignUp component on click

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Hero.css';
 
-const Hero = () => {
+const Hero = ({ onGetStartedClick }) => {
   return (
     <div className="bg-container">
       <div className="bg-overlay">
@@ -9,11 +9,11 @@ const Hero = () => {
           <h1 className='hero-title'>PROGRAMMER
           <br/>BUBBLE</h1>
           <p className="hero-subtitle">Dive into the Code. Join the Bubble.</p>
-          <button className='hero-button'>GET STARTED</button>
+          <button className='hero-button' onClick={onGetStartedClick}>GET STARTED</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
