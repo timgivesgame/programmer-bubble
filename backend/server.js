3d39e1d135c932936171432313654364e5b18500
timgivesgame <142540848+timgivesgame@users.noreply.github.com>
2024-05-26 08:36:28 -0400

    add: moved env

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { OAuth2Client } = require('google-auth-library');
+const admin = require('firebase-admin');
 require('dotenv').config();
-const credentials = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CREDENTIALS);
 
 
 const app = express();
@@ -16,6 +16,15 @@ app.use(bodyParser.json());
 // Google OAuth client
 const CLIENT_ID = '459741616357-k1h92ul4h6q33rp7v28eud7fepmfjnp7.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
+
+console.log(process.env.FIREBASE_ADMIN_SDK_CREDENTIALS + 'here')
+// Initialize Firebase Admin SDK
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://programmerbubble-424508-default-rtdb.firebaseio.com/' // Replace with your Firestore database URL
+});
 
 app.post('/api/signup', async (req, res) => {
     const { email, firstName, lastName } = req.body;
@@ -32,55 +41,8 @@ app.post('/api/signup', async (req, res) => {
     }
 });
 
-
-app.get('/api/signup', (req, res) => {
-    res.status(405).json({ error: 'Method Not Allowed', message: 'This endpoint only accepts POST requests' });
-});
-
-app.post('/api/google-signin', async (req, res) => {
-    const { token } = req.body;
-
-    try {
-        // Verify Google token...
-        // Assuming you have obtained user data from the token
-        const userData = {
-            email: 'example@gmail.com',
-            firstName: 'John',
-            lastName: 'Doe',
-            // Add more user data as needed
-        };
-
-        // Check if user already exists in Firestore
-        const userDoc = await admin.firestore().collection('users').doc(userData.email).get();
-        if (!userDoc.exists) {
-            // If user doesn't exist, create new user document
-            await admin.firestore().collection('users').doc(userData.email).set({
-                firstName: userData.firstName,
-                lastName: userData.lastName,
-                // Add more user data as needed
-            });
-        }
-
-        res.status(200).send({ message: 'Google sign-in successful', user: userData });
-    } catch (error) {
-        res.status(400).send({ message: 'Error signing in with Google', error });
-    }
-});
-
+// Other routes...
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-
-
-
-//Firebase
-
-const admin = require('firebase-admin');
-
-const serviceAccount = require(credentials);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://programmerbubble-424508-default-rtdb.firebaseio.com/' // Replace with your Firestore database URL
 });
